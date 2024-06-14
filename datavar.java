ff6bd533014bc31657c8b176045bc4ad6f3f9ab4
prabhaharncad01 <prabhaharncad01@gmail.com>
2024-06-14 20:10:04 +0530

    added java practice programs

---
+++ datavar.java
@@ -0,0 +1,13 @@
+class datavar{
+    public static void main(String args[]){
+        int container=57;
+        String container2="prabaharan";
+        int a=10;
+        int b=20;
+        
+        
+        System.out.print(container);
+        System.out.print(a+b);
+        System.out.print(container2);
+    }
+}
---
+++ userip.java
@@ -0,0 +1,13 @@
+import java.lang.System;
+import java.util.Scanner;
+
+
+class userip{
+    public static void main(String args[])
+    {
+Scanner praba = new Scanner(System.in);
+int a= praba.nextInt();
+int b= praba.nextInt();
+System.out.print(a+b);
+    }
+}
