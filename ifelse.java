3bc9860178897f58e8da3e98ff2dc72b9ac7c494
prabhaharncad01 <prabhaharncad01@gmail.com>
2024-06-15 07:47:46 +0530

    practised  ifelse javaprogram

---
+++ ifelse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+class ifelse{
+    public static void main(String args[]){
+        Scanner ifelse =new Scanner (System.in);
+        int a=ifelse.nextInt();
+        int b=ifelse.nextInt();
+        
+        if(a==b){
+System.out.print("numbers are equal");
+        }
+
+        else{
+            System.out.print("numbers are not equal");
+        }
+    }
+}
